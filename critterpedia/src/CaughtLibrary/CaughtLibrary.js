50b955872a859c89969d764bd3ab232526375114
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-02-15 13:29:13 -0800

    adding classNames for styling

--- critterpedia/src/CaughtLibrary/CaughtLibrary.js
+++ critterpedia/src/CaughtLibrary/CaughtLibrary.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const CaughtLibrary = (props) => {
+
+    const list = props.caught.map((critterData, index) => {
+        return (
+            <img className='critter-icon caught' key={index} src={critterData.icon_uri} alt='icon'/>
+        )
+    })
+
     return (
         <div>
-        Caught Library
+            <h4>Caught critters!</h4>
+            <div className='caught-library'>
+                {list}
+            </div>
         </div>
     )
 }
