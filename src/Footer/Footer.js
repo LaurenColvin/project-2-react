95cfbc70f8c7d2eed108b7a08c8ecba9fcfaa0db
Lauren Colvin <lauren@Laurens-MacBook-Pro-2.local>
2022-02-17 16:27:07 -0800

    adding footer component

---
+++ src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <h6>Designed and created by <a href="https://www.linkedin.com/in/laurenleighcolvin/">Lauren Colvin</a></h6>
+            <h6>All data sourced from: <a className="api-link" href='https://acnhapi.com/'>Animal Crossing API</a></h6>
+        </footer>
+    )
+}
+
+export default Footer;
